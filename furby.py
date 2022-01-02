b6ccb4e916571e1081442ca13c42715a7aca6c2c
Richard Kraaijenhagen <stdin@riichard.com>
2022-01-02 20:48:30 +0100

    push

--- furby.py
+++ furby.py
@@ -3,7 +3,7 @@
 # Import required modules
 import time
 import RPi.GPIO as GPIO
-
+import random
 GPIO.cleanup()
 
 # Declare the GPIO settings
@@ -32,14 +32,6 @@ IR = 18 # 24
 CAL = 36 # 16
 PWMA_TEST = 32 # PWM0 BCM12
 TONGUE = 33
-
-# GPIO.setup(IR, GPIO.IN)
-GPIO.setup(IR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(CAL,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(TONGUE,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-
-GPIO.setup(PWMA_TEST, GPIO.OUT)
-pwm = GPIO.PWM(PWMA_TEST, 300)
 
 ir_ticks = 0
 def ir_callback(channel):
@@ -135,9 +127,152 @@ def foo():
         else:
             stop_furby()
 
+class Furby:
+    def __init__(self):
+        self.pos = 0
+        self.maxPos = 100
+        self.clockwise = True
+        self.calibrated = False
+        self.des = 0
+
+        # GPIO.setup(IR, GPIO.IN)
+        GPIO.setup(IR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(CAL,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(TONGUE,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+        GPIO.setup(PWMA_TEST, GPIO.OUT)
+        self.pwm = GPIO.PWM(PWMA_TEST, 300)
+
+        GPIO.setup(PWMA, GPIO.OUT) # Connected to PWMA 
+        GPIO.setup(AIN2, GPIO.OUT) # Connected to AIN2
+        GPIO.setup(AIN1, GPIO.OUT) # Connected to AIN1
+        GPIO.setup(STDBY, GPIO.OUT) # Connected to STBY
+
+        GPIO.add_event_detect(CAL,GPIO.RISING,callback=self.calCallback, bouncetime=200) 
+        GPIO.add_event_detect(TONGUE,GPIO.RISING,callback=self.tongueCallback, bouncetime=200) 
+        GPIO.add_event_detect(IR,GPIO.RISING,callback=self.irCallback, bouncetime=200) 
+
+        self.setDirection(True)
+
+    def start(self):
+        print("starting")
+        GPIO.output(STDBY, GPIO.HIGH)
+        self.pwm.start(60)
+
+    def stop(self):
+        print("stopping")
+        self.pwm.stop()
+        GPIO.output(STDBY, GPIO.LOW)
+        print("maxpos", self.maxPos)
+
+    def setDirection(self, clockwise):
+        if clockwise:
+            print("Setting direction clockwise")
+            GPIO.output(AIN1, GPIO.HIGH) # Set AIN1
+            GPIO.output(AIN2, GPIO.LOW) # Set AIN2
+        else:
+            print("Setting direction counter clockwise")
+            GPIO.output(AIN1, GPIO.LOW) # Set AIN1
+            GPIO.output(AIN2, GPIO.HIGH) # Set AIN2
+        self.clockwise = clockwise
+    
+    def setSpeed(self, speed): # 0 < speed < 100
+        pwm.ChangeDutyCycle(speed)
+
+    def calCallback(self, pin):
+        print("cal callback " + str(self.pos))
+        self.calibrated = True
+        self.pos = 0
+
+    def irCallback(self, pin):
+        if self.maxPos < self.pos:
+            self.maxPos = self.pos
+
+        if self.clockwise:
+            self.pos += 1
+            if self.pos >= self.des:
+                self.stop()
+        else:
+            self.pos -= 1
+            if self.pos <= self.des:
+                self.stop()
+        # print("ir callback " + str(self.pos))
+
+    def tongueCallback(self, pin):
+        print("tongue callback")
+
+    def moveDown(self, des):
+        print("moving down, "+str(des))
+        self.setDirection(False)
+
+        self.des = des
+        self.start()
+
+    def moveUp(self, des):
+        print("moving up, "+str(des))
+        self.setDirection(True)
+        print("moving up")
+
+        self.des = des
+        self.start()
+        print("destination reached")
+
+    def moveTo(self, des):
+        print(self.pos, des)
+        if des > self.pos:
+            self.moveUp(des)
+        else:
+            self.moveDown(des)
+
+    def calibrate(self):
+        print("calibrating")
+        self.pos = -500
+        self.moveTo(-10)
+        print("calibrated")
+        """
+        self.setDirection(False)
+        self.calibrated = False
+        time.sleep(0.01)
+        self.start()
+        while self.calibrated == False:
+            print("calibrating..")
+            time.sleep(0.01)
+        print("cal reached")
+        self.stop()
+        self.calibrated = False
+        """
+
 if __name__ == '__main__':
     try:
-        main()
+        f = Furby()
+
+        #f.start()
+        #time.sleep(10)
+        #f.stop()
+        f.calibrate()
+
+        """
+        #r = random.randint(0,f.maxPos)
+        f.moveTo(r)
+        time.sleep(1)
+        f.calibrate()
+        f.moveTo(0)
+        time.sleep(1)
+        f.moveTo(100)
+        time.sleep(1)
+        f.moveTo(200)
+        """
+
+
+        #time.sleep(1)
+        #f.moveTo(10)
+        #time.sleep(1)
+        #f.moveTo(60)
+        # f.moveTo(30)
+        # f.moveTo(60)
+
+        
+
     except KeyboardInterrupt:
         print("Cleanup by keyboard interrupt")
         GPIO.cleanup()
